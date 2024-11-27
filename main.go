62e98f54d698d7ca8209e498e1f2bbc976567582
Antoine Racine <me@aracine.net>
2024-11-27 13:53:53 -0500

    add project scaffolding with dummy endpoints

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("alive"))
+	})
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	r.Route("/{vendor}", func(r chi.Router) {
+		r.Post("/receive", func(w http.ResponseWriter, r *http.Request) {
+			if vendor := chi.URLParam(r, "vendor"); vendor != "" {
+				w.Write([]byte(fmt.Sprintf("received for %s", vendor)))
+			} else {
+				w.Write([]byte("received at top level"))
+			}
+		})
+
+	})
+	http.ListenAndServe(":3000", r)
+}
