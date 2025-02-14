4fc73f1bd96a414e212f907b7a503eb6791593c5
Antoine Racine <me@aracine.net>
2025-02-14 13:23:39 -0500

    remove test code from main file

--- main.go
+++ main.go
@@ -5,21 +5,9 @@ package main
 
 import (
 	"binarytiger/jolly_roger/cmd"
-	"database/sql"
-	"fmt"
 
-	_ "github.com/ncruces/go-sqlite3/driver"
-	_ "github.com/ncruces/go-sqlite3/embed"
 )
 
 func main() {
-	//saveRawHook()
 	cmd.Execute()
-}
-
-func saveRawHook() {
-	var version string
-	db, _ := sql.Open("sqlite3", "file:local.db") // #TODO load as config
-	db.QueryRow(`SELECT sqlite_version()`).Scan(&version)
-	fmt.Println(version)
 }
